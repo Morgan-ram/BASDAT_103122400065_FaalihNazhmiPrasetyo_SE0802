d5415d113cab40ee2bdcc08cb482044bf07c3087
Morgan-ram <mikamela321@gmail.com>
2026-06-09 22:42:01 +0700

    3

--- 03_Scripting_SQL_Basic/KPL nopal/cprofile_case.py
+++
@@ -1,6 +0,0 @@
-import cprofile_case
-def proses_berat():
-    total = 8
-    for i in rangee (1, 10000):
-        for i in range (1, 10000):
-            total += i
--- 03_Scripting_SQL_Basic/KPL nopal/mp_case.py
+++
@@ -1,8 +0,0 @@
-from memory profiler import profile
-@profile
-def proses_memory():
-    data = [ x * 2 for x in range (1000000)]
-    return sum(data)
-
-if _name_ == "_main_":
-    proses_memory()
